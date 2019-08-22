290625c252e1ca98ffe564e2c70e56dbb9b5031c
nickvarberg <nick.varberg@my.wheaton.edu>
2019-08-21 18:37:06 -0600

    imporvements

--- FF Draft Algo.R
+++ FF Draft Algo.R
@@ -1,12 +1,13 @@
 library(data.table)
 library(dplyr)
-ffa <- fread("/Users/nickvarberg/Downloads/FFA_projections - Sheet1.csv")
+cols <- c('Player', 'Pos', 'Points', 'Floor', 'Ceiling', 'ADP')
+ffa <- fread("/Users/nickvarberg/Downloads/FFA_projections - Sheet1.csv", col.names=cols)
 # Create draft position DP variable
 ffa <- ffa %>% arrange(ADP) %>% tibble::rowid_to_column("DP")
 ffa <- ffa %>% group_by(Pos) %>% arrange(DP) %>% 
   mutate(posrank=order(DP)) %>% ungroup()
 
-# Attributes of snake draft
+#Attributes of snake draft
 positions       <- c('QB', 'RB', 'WR', 'TE')
 PICKS_PER_ROUND <- 8
 my_picks        <- c(6,11,22,27,38,43,54,59,70)
@@ -22,20 +23,34 @@ ffa <- mutate(ffa, points_arp = ifelse(Pos=='RB', Points-RB_REPL_POINTS,
                                 ifelse(Pos=='WR', Points-WR_REPL_POINTS,
                                 ifelse(Pos=='TE', Points-TE_REPL_POINTS, 0)
                                 ))))
+tbl_list <- c()
 for (pick in my_picks){
-  pot_players <- ffa %>% filter(DP>=pick & DP <= pick+2*PICKS_PER_ROUND)
-  for(pos in positions){
-    pot_players_pos <- filter(pot_players, Pos==pos)
-    if (dim(pot_players_pos[1] > 0)){
-      max_points_pos <- max(pot_players_pos$Points)
-    }
-    else{
-      max_points_pos = NA
-    }
-    # Nick: assign max_points_pos (or really points above ave points of other pos)
-    # (i.e. pos_value_round) to a dataframe called pick_values for each position
-  }
+  # Filter to plyers in the next 2 rounds after pick
+  pick_vals <- ffa %>% 
+    filter(DP>=pick & DP <= pick+2*PICKS_PER_ROUND) %>% 
+    group_by(Pos) %>% 
+    filter(points_arp == max(points_arp)) %>%
+    mutate(pick = pick) %>%
+    mutate(posvalue = (points_arp - max(points_arp))) %>%
+    select(pick, Player, Pos, posrank, Points, DP, points_arp, posvalue) %>%
+    arrange(desc(points_arp)) %>%
+    ungroup() %>%
+    mutate(points_arp_value = (points_arp - mean(points_arp)))
+  tbl_list <- c(tbl_list, pick_vals)
+  #row_add <- c(max_vals$QB, max_vals$RB, max_vals$WR, max_vals$TE)
+  #for(pos in 'RB'){
+    # Filter to the players at the position of interest
+    #pot_players_pos <- filter(pot_players, Pos==pos)
+    #if (dim(pot_players_pos[1] > 0)){
+    #  max_points_pos <- max(pot_players_pos$Points)
+    #}
+    #else{
+    #  max_points_pos = NA
+    #}
+  #}
 }
+
+
 
 # Nick: create function, linear or logistic for points expected at pick.
 #  Then calculate points above expected at the pick.
