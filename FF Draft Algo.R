882fb7393a858cae63c2050ca1c23391050a061b
nickvarberg <nick.varberg@my.wheaton.edu>
2019-08-23 09:01:07 -0600

    worked on scraping data

--- FF Draft Algo.R
+++ FF Draft Algo.R
@@ -20,13 +20,13 @@ draft_attributes <- function(){
   names(positions) <- c('QB', 'RB', 'WR', 'TE')
   assign('positions', positions, envir=.GlobalEnv)
   assign('PICKS_PER_ROUND', 8, envir=.GlobalEnv)
-  assign('my_picks', c(3,14,19,30,35,46,51,62,67), envir=.GlobalEnv)
+  assign('MY_PICKS', c(3,14,19,30,35,46,51,62,67), envir=.GlobalEnv)
 }
 draft_attributes()
 
 calculate_parp <- function(projections){
   # My last pick is the replacement level pick
-  REPL_PICK        <- tail(my_picks,1) 
+  REPL_PICK        <- tail(MY_PICKS,1) 
   # Replaceent points are max points you could get at that position in the last round
   RB_REPL_POINTS <- max(filter(ffa, Pos=='RB' & DP >= REPL_PICK)$Points)
   QB_REPL_POINTS <- max(filter(ffa, Pos=='QB' & DP >= REPL_PICK)$Points)
@@ -41,41 +41,30 @@ calculate_parp <- function(projections){
 }
 projections <- calculate_parp(ffa)
 
-#### Start of Draft
+
 # Update who I've drafted and who other's have drafted (ShinyApp)
 my_drafted <- NULL
-others_drafted <- NULL
+others_drafted <- slice(projections, 21)
 
+#### Start of Rest-of-Draft Simulation
 # Reduce to my picks remaining
-my_picks_remain <- function(my_picks, my_drafted){
-  if(is.null(my_drafted)){
-    return(my_picks)
-  }
-  else{
-    return(setdiff(my_picks, my_drafted$pick))
-  }
+get_picks_remain <- function(my_picks, my_drafted){
+  if(is.null(my_drafted)){ return(my_picks)
+  } else{ return(setdiff(my_picks, my_drafted$pick))}
 }
-my_picks_remain <- my_picks_remain(my_picks, my_drafted)
-
 # Filter to players available
-players_avail   <- function(projections, my_drafted, others_drafted){
-  if(is.null(my_drafted)){
-    players_avail <- projections
-  }
-  else{
+get_players_avail <- function(projections, my_drafted, others_drafted){
+  if(is.null(my_drafted)){ players_avail <- projections
+  } else{
     players_avail <- projections %>% filter(!Player %in% my_drafted$Player)
   }
-  if(is.null(others_drafted)){
-    players_avail <- players_avail
-  }
-  else{
+  if(is.null(others_drafted)){ players_avail <- players_avail
+  } else{
     players_avail <- players_avail %>% filter(!Player %in% others_drafted$Player)
   }
 }
-players_avail <- players_avail(projections, my_drafted, others_drafted)
-
-# get_pick_values: Function to return values for each position at each pick with the players remaining
-pick_values <- function(players_avail, my_picks_remain){
+# Return value of each position at each pick among players remaining
+get_pick_values <- function(players_avail, my_picks_remain){
   for (pick in my_picks_remain){
     # Filter to plyers in the next 2 rounds after pick
     df <- players_avail %>% 
@@ -85,7 +74,8 @@ pick_values <- function(players_avail, my_picks_remain){
       mutate(pick = pick) %>%
       arrange(desc(points_arp)) %>%
       ungroup() %>%
-      mutate(value = (points_arp - round(mean(points_arp),2)))
+      mutate(value = (points_arp - round(mean(points_arp),2))) %>%
+      select('pick', 'Player', 'Pos', 'Points', 'points_arp', 'DP', 'value')
     if (pick == my_picks_remain[1]){
       pick_values <- df
     }
@@ -95,27 +85,9 @@ pick_values <- function(players_avail, my_picks_remain){
   }
   return(arrange(pick_values, desc(value)))
 }
-pick_values <- pick_values(players_avail, my_picks_remain)
-
-# sim_my_draft: Function thats arranges by value and chooses best values
-sim_my_draft <- function(pick_values){
-  # Initialize variables and select first choice
-  sim_my_picks_remain <- my_picks_remain
-  sim_my_drafted <- slice(best_avail, 1)
-  # Draft rest of round
-  while (nrow(sim_my_drafted) < nrow(sim_my_picks_remain)){
-    sim_my_picks_remain <- my_picks_remain(sim_my_picks_remain, sim_my_drafted) 
-    pos_filled     <- pos_filled(my_drafted, sim_my_drafted)
-    if(!is.null(pos_filled)){
-      
-    }
-    
-  }
-}
-
-pos_filled <- function(my_drafted, sim_my_drafted){
-  full_drafted   <- full_join(my_drafted, sim_my_drafted, by = 'Player')
-  df             <- drafted %>% group_by(Pos) %>% summarise(n = n())
+# Check if postions are filled by my drafted players
+get_pos_filled <- function(all_drafted){
+  df             <- all_drafted %>% group_by(Pos) %>% summarise(n = n())
   num_pos        <- df %>% pull(n)
   names(num_pos) <- df %>% pull(Pos)
   pos_filled     <- c()
@@ -126,3 +98,35 @@ pos_filled <- function(my_drafted, sim_my_drafted){
   }
   return(pos_filled)
 }
+
+# Choose best value at any pick first, then recalculate values
+sim_my_draft <- function(projections, my_picks_remain){
+  # Initialize variables and select first choice
+  my_picks_remain <- get_picks_remain(MY_PICKS, my_drafted)
+  players_avail <- get_players_avail(projections, my_drafted, others_drafted)
+  pick_values <- get_pick_values(players_avail, my_picks_remain)
+  sim_my_drafted <- slice(pick_values, 1)
+  my_picks_remain <- get_picks_remain(my_picks_remain, sim_my_drafted)
+  # Draft rest of round
+  while (length(my_picks_remain) > 0){
+    if(is.null(my_drafted)){all_drafted <- sim_my_drafted 
+    } else{all_drafted <- full_join(my_drafted, sim_my_drafted, by='Player')}
+    players_avail  <- players_avail %>% filter(!Player %in% all_drafted$Player)
+    pos_filled     <- get_pos_filled(all_drafted)
+    if(!is.null(pos_filled)){
+      players_avail <- players_avail %>% filter(!Pos %in% pos_filled)
+    }
+    pick_values <- get_pick_values(players_avail, my_picks_remain)
+    sim_my_drafted <- full_join(sim_my_drafted, slice(pick_values,1))
+    my_picks_remain <- get_picks_remain(my_picks_remain, sim_my_drafted)
+  }
+  print(arrange(sim_my_drafted, pick))
+}
+
+# test
+sim_my_draft(projections, my_picks_remain)
+
+##
+library('shiny')
+runExample('01_Hello')
+runApp("FF_Draft_Algo_Shiny")
