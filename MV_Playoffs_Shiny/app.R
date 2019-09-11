3acfdfa61138240474c7942289bbd1c114491099
nickvarberg <nick.varberg@my.wheaton.edu>
2019-09-10 21:34:26 -0600

    published MV with plot

--- MV_Playoffs_Shiny/app.R
+++ MV_Playoffs_Shiny/app.R
@@ -27,7 +27,7 @@ ui <- fluidPage(
                 h6("I gather preseason point projections for each player from fantasyfootballanlytics.net, who claim
                    to have the most accurate projections. From these player projections, I project the points per game (proj_ppg) for each team
                    in our league. I model each team's score each remaining week as a normal distribution with their
-                   proj_ppg as the mean and 25 (arbitrary) as the standard deviation. Then I similate the rest of the
+                   proj_ppg as the mean and 30 (arbitrary) as the standard deviation. Then I similate the rest of the
                    season 30,000 times to determine how far each team is likely to go.", align = 'center'),
                 
                 tabsetPanel(
@@ -43,7 +43,7 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$preds2019 <- DT::renderDataTable({
-     df <- fread("MV_predictions_week_0_2019.csv")
+     df <- fread("MV_predictions_week_1_2019.csv")
      df <- df %>% select(-ID) %>% mutate('Playoffs' = percent(Playoffs)) %>% 
        mutate('Semifinals' = percent(Semifinals)) %>%
        mutate('Finals' = percent(Finals)) %>%
@@ -54,10 +54,14 @@ server <- function(input, output) {
    
    output$Historyplot <- renderPlot({
      history <- fread("MV_predictions_history_2019.csv")
+     history <- history %>% select(-ID) %>% mutate('Playoffs' = percent(Playoffs)) %>% 
+       mutate('Semifinals' = percent(Semifinals)) %>%
+       mutate('Finals' = percent(Finals)) %>%
+       mutate('Champion' = percent(Champion))
      p <- ggplot(history, aes(x=Week, y=Playoffs, group=Owner) ) + 
        geom_line(aes(color=Owner)) +
-       geom_point(aes(color=Owner)) +
-       scale_x_discrete(breaks=0:11, labels=waiver())
+       geom_point(aes(color=Owner)) #+
+       #scale_x_discrete(breaks=0:11, labels=waiver())
      return(p)
    })
    
